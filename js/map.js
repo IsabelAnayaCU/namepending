8da37f6c594ef8b8ce42797882e4e9071606f3ad
Pourna Sengupta <pourna.sengupta@colorado.edu>
2020-10-14 06:04:26 -0600

    initial commit. Completed Pages: Login, HealthQ

---
+++ js/map.js
@@ -0,0 +1,28 @@
+/*MAP TAB JS FORMATTING */
+
+/*
+//Get the Modal 
+var norlinModal = document.getElementById("norlin_modal"); 
+
+//Get image 
+var norlinImg = document.getElementById("norlin"); 
+//Insert into modal 
+var norlinModalImg = document.getElementById("img001"); 
+//Use alt text as caption 
+var captionText = document.getElementById("caption"); 
+
+//on click functionality 
+area.onclick = function() {
+    modal.style.display = "block"; 
+    norlinModalImg.src = this.src; 
+    captionText.innerHTML = this.alt; 
+}
+
+//Span element to close modal 
+var span = document.getElementsByClassName("close")[0]; 
+
+//when user clicks on x, modal closes 
+span.onclick = function() {
+    modal.style.display = "none"; 
+}*/
+
