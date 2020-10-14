7135f1ca53a49e6eba297120b5fb933174f55818
Pourna Sengupta <pourna.sengupta@colorado.edu>
2020-10-14 10:22:21 -0600

    attempting to remove map styles. working backwards to see if I can get the map back onto the webpage.

--- js/map.js
+++ js/map.js
@@ -2,12 +2,12 @@
 
 /*https://account.mapbox.com: make the map appear using access token from account */
 
-mapboxgl.accessToken = '<pk.eyJ1IjoicG91cm5hcyIsImEiOiJja2c5ZGl0MnEwNzh4MnRwNW84aWRlMzA2In0.igXHhRH5VJlv4lTfOqii6g>';
 
 //DISPLAY CU BOULDER CAMPUS MAP 
+    mapboxgl.accessToken = 'pk.eyJ1IjoicG91cm5hcyIsImEiOiJja2c5ZGl0MnEwNzh4MnRwNW84aWRlMzA2In0.igXHhRH5VJlv4lTfOqii6g';
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
-        zoom: 15,
-        center: [-105.2656199, 40.0049445] 
+        center: [-105.2656199, 40.0049445], 
+        zoom: 15
     });
