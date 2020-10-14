b8a326b37e5a2334e01e1f0361719009249511d6
Pourna Sengupta <pourna.sengupta@colorado.edu>
2020-10-14 07:17:23 -0600

    added map

--- js/map.js
+++ js/map.js
@@ -2,11 +2,11 @@
 
 /*https://account.mapbox.com: make the map appear using access token from account */
 
-mapboxgl.accessToken = '<sk.eyJ1IjoicG91cm5hcyIsImEiOiJja2c5ZHE4aDQwcTBwMnJtdTBiMjcyb2gyIn0.MPNsbHaLvewsgwIkz9lKtQ>'; 
+/*mapboxgl.accessToken = '<sk.eyJ1IjoicG91cm5hcyIsImEiOiJja2c5ZHE4aDQwcTBwMnJtdTBiMjcyb2gyIn0.MPNsbHaLvewsgwIkz9lKtQ>'; 
 
 var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: [-74.5, 40], // starting position [lng, lat]
     zoom: 9 // starting zoom
-});
+});*/
