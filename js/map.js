8db78988037f8a4f3d22eb63011f87f63c6d9635
Pourna Sengupta <pourna.sengupta@colorado.edu>
2020-10-14 10:33:58 -0600

    rewrote map.html with inline css and js, removed map.css and map.js links/scripts

--- js/map.js
+++ js/map.js
@@ -2,12 +2,3 @@
 
 /*https://account.mapbox.com: make the map appear using access token from account */
 
-
-//DISPLAY CU BOULDER CAMPUS MAP 
-    mapboxgl.accessToken = 'pk.eyJ1IjoicG91cm5hcyIsImEiOiJja2c5ZGl0MnEwNzh4MnRwNW84aWRlMzA2In0.igXHhRH5VJlv4lTfOqii6g';
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [-105.2656199, 40.0049445], 
-        zoom: 15
-    });
